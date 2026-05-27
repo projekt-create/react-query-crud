97ca7eb14a0962afd80844e0636808d65dcd0059
Husan <kilua9389@gmail.com>
2026-05-26 21:15:56 -0500

    Done

--- src/api/ProductsApi.js
+++ src/api/ProductsApi.js
@@ -2,25 +2,21 @@ import axios from 'axios'
 
 const API = 'http://localhost:3002/products'
 
-// GET
 export const getProducts = async () => {
   const res = await axios.get(API)
   return res.data
 }
 
-// ADD
 export const addProduct = async (newProduct) => {
   const res = await axios.post(API, newProduct)
   return res.data
 }
 
-// DELETE
 export const deleteProduct = async (id) => {
   await axios.delete(`${API}/${id}`)
   return id
 }
 
-// EDIT
 export const updateProduct = async (updatedProduct) => {
   const res = await axios.put(
     `${API}/${updatedProduct.id}`,
